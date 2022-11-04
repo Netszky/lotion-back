c2042c85dce91901b874e1f7abb18600f7ca302a
Netszky <julien.chigot@ynov.com>
2022-11-04 15:23:04 +0100

    cors

--- src/services/express.service.js
+++ src/services/express.service.js
@@ -6,7 +6,8 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(cors({ origin: "*" }));
+// app.use(cors({ origin: "*" }));
+app.use(cors());
 
 app.use(function (req, res, next) {
   if (req.originalUrl === '/api/v1/stripe') {
