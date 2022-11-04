5b13e749829d280ecf12f72e659f8b0024ae033a
Léo Peyré <leo.peyre95@gmail.com>
2022-11-04 15:44:56 +0100

    log

--- src/controllers/webhooks.controller.js
+++ src/controllers/webhooks.controller.js
@@ -72,7 +72,7 @@ exports.stripewebhook = (req, res) => {
       break;
     case "customer.subscription.deleted":
       const customerSubscriptionDeleted = data.object;
-      console.log("Je suis customerSubscriptionDeleted = ",customerSubscriptionDeleted);
+      console.log("Je suis customerSubscriptionDeleted.create_date = ",customerSubscriptionDeleted.create_date);
       GetDateEndToSub(customerSubscriptionDeleted.create_date)
       User.findByIdAndUpdate(
         customerSubscriptionDeleted.metadata.userId,
