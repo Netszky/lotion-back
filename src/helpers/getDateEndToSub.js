216194d3dbdfe5129efd1108730ea0e2476bc4bb
Léo Peyré <leo.peyre95@gmail.com>
2022-11-04 15:42:47 +0100

    log

--- src/helpers/getDateEndToSub.js
+++ src/helpers/getDateEndToSub.js
@@ -3,13 +3,17 @@ const getDateEndToSub = (createdDate) => {
     console.log("Je suis dans la fonction");
     const dateCreated = new Date(createdDate);
 
+    console.log("Je suis dateCreated = ",dateCreated);
+
     const date = new Date();
+
+    console.log("Je suis today = ",today);
     
     const month = dateCreated.getMonth();
     
     let day = date.getDate();
     
-    const newDate = new Date(`${month + 2}-${day }`)
+    const newDate = new Date(`${month + 2}-${day}`)
     
     console.log("Je suis newDate = ",newDate)
 }
