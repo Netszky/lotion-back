1f1ca09acbd3defb374ff895dc7dd42a89421979
Netszky <julien.chigot@ynov.com>
2022-11-17 11:25:06 +0100

    dev

--- src/helpers/verifDateSub.js
+++ src/helpers/verifDateSub.js
@@ -6,17 +6,19 @@ const verifDateSub = () => {
     .populate("subscription")
     .then((users) => {
       users.forEach(async (user) => {
-        if (new Date() >= user.subscription?.cancel_date) {
-          console.log("email to delete", user.email);
-          const exist = await Subscription.exists({ _id: user.subscription._id })
-          if (exist) {
-            await Subscription.findByIdAndDelete(user.subscription._id).then((data) => {
-              console.log("user to delete", data)
-              User.findByIdAndUpdate(user._id,
-                { isSub: false }, { new: true, omitUndefined: true }
-              ).then((data) => console.log(data))
+        if (user.subscription.cancel_date) {
+          if (new Date() >= user.subscription?.cancel_date) {
+            console.log("email to delete", user.email);
+            const exist = await Subscription.exists({ _id: user.subscription._id })
+            if (exist) {
+              await Subscription.findByIdAndDelete(user.subscription._id).then((data) => {
+                console.log("user to delete", data)
+                User.findByIdAndUpdate(user._id,
+                  { isSub: false }, { new: true, omitUndefined: true }
+                ).then((data) => console.log(data))
+              }
+              );
             }
-            );
           }
         }
       });
