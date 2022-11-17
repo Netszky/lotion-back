394a3093c287aff7db54bd99a12014333be600ed
Netszky <julien.chigot@ynov.com>
2022-11-17 12:19:11 +0100

    test

--- src/helpers/verifDateSub.js
+++ src/helpers/verifDateSub.js
@@ -6,28 +6,29 @@ const verifDateSub = () => {
     .populate("subscription")
     .then((users) => {
       users.forEach(async (user) => {
-        if (user.subscription.cancel_date != null || user.subscription.cancel_date !== undefined) {
-          console.log(user.email);
-          if (new Date() >= user.subscription.cancel_date) {
-            console.log("email to delete", user.email);
-            const exist = await Subscription.exists({ _id: user.subscription._id })
-            if (exist) {
-              await Subscription.findByIdAndDelete(user.subscription._id).then((data) => {
-                console.log("user to delete", data)
-                User.findByIdAndUpdate(user._id,
-                  { isSub: false }, { new: true, omitUndefined: true }
-                ).then((data) => console.log(data))
-              }
-              );
-            }
-          } else {
-            console.log("SIIIIIIIIIOUF");
-            return
-          }
-        } else {
-          console.log("pas user");
-          return
-        }
+        console.log(user.subscription);
+        // if (user.subscription.cancel_date != null || user.subscription.cancel_date !== undefined) {
+        //   console.log(user.email);
+        //   if (new Date() >= user.subscription.cancel_date) {
+        //     console.log("email to delete", user.email);
+        //     const exist = await Subscription.exists({ _id: user.subscription._id })
+        //     if (exist) {
+        //       await Subscription.findByIdAndDelete(user.subscription._id).then((data) => {
+        //         console.log("user to delete", data)
+        //         User.findByIdAndUpdate(user._id,
+        //           { isSub: false }, { new: true, omitUndefined: true }
+        //         ).then((data) => console.log(data))
+        //       }
+        //       );
+        //     }
+        //   } else {
+        //     console.log("SIIIIIIIIIOUF");
+        //     return
+        //   }
+        // } else {
+        //   console.log("pas user");
+        //   return
+        // }
       })
     });
 };
